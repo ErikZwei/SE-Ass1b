2175925334ddb5a43dc01a1a8d696670e6665e20
steisleis <borgesteinar@gmail.com>
2015-04-28 17:39:50 +1200

    asdf

--- SERLER/src/assignment1b/Main.java
+++ SERLER/src/assignment1b/Main.java
@@ -5,6 +5,9 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hei Erik!");
+		System.out.println("Hei Øyvind!");
+		System.out.println("Hei Ingvild!");
+		
 	}
 
 }
